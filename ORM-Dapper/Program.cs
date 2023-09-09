434695b75023afbb4cd15bc18f495de86ca84f5a
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-09-09 18:16:46 -0400

    Completed Exercise 2 with bonus

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -4,24 +4,37 @@ using MySql.Data.MySqlClient;
 using ORM_Dapper;
 using System.Data;
 
-var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
 
-string connString = config.GetConnectionString("DefaultConnection");
+    var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
 
-IDbConnection conn = new MySqlConnection(connString);
+    string connString = config.GetConnectionString("DefaultConnection");
+
+    IDbConnection conn = new MySqlConnection(connString);
 
 
-var departmentRepo = new DapperDepartmentRepository(conn);
+    var departmentRepo = new DapperDepartmentRepository(conn);
 
-departmentRepo.InsertDepartment("New Department");
+    departmentRepo.InsertDepartment("New Department");
 
-var departments = departmentRepo.GetAllDepartments();
+    var departments = departmentRepo.GetAllDepartments();
 
-foreach (var department in departments)
-{
-    Console.WriteLine(department.DepartmentID);
-    Console.WriteLine(department.Name);
-}
+    foreach (var department in departments)
+    {
+        Console.WriteLine(department.DepartmentID);
+        Console.WriteLine(department.Name);
+    }
+
+    var repo = new DapperProductRepository(conn);
+    var products = repo.GetAllProducts();
+
+    repo.CreateProduct("newProduct", 20, 1);
+
+    foreach (var product in products)
+    {
+        Console.WriteLine($"{product.ProductID} {product.Name}" );
+    }
+
+    
